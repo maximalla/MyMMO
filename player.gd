a2fc4be46801fd84f161316da0be2b5f9eb086a4
Messst <folmyt@gmail.com>
2026-02-14 17:35:04 +0200

    committ

---
+++ player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+@export var speed: float = 250.0
+
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+
+func _physics_process(delta):
+	if !is_multiplayer_authority():
+		return
+
+	var dir = Vector2.ZERO
+	dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	velocity = dir.normalized() * speed
+	move_and_slide()
+
+	rpc("sync_position", global_position)
+
+@rpc("unreliable")
+func sync_position(pos: Vector2):
+	if !is_multiplayer_authority():
+		global_position = pos
