d73e79a142a2f720c2696ae431441cb128255f67
Maksim Shpontak <maximalla1997@gmail.com>
2026-02-15 14:56:57 +0200

    fix spawn server character

--- player.gd
+++ player.gd
@@ -6,27 +6,29 @@ var input_dir: Vector2 = Vector2.ZERO
 
 func _physics_process(delta):
 
-	# Клієнт відправляє інпут серверу
 	if is_multiplayer_authority():
+
 		var dir = Vector2.ZERO
 		dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 		dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 
-		rpc_id(1, "receive_input", dir)  # 1 = сервер
+		if multiplayer.is_server():
+			# якщо це хост — напряму
+			input_dir = dir
+		else:
+			# якщо клієнт — шлемо серверу
+			rpc_id(1, "receive_input", dir)
 
-	# Сервер рухає ВСІХ
 	if multiplayer.is_server():
 		velocity = input_dir.normalized() * speed
 		move_and_slide()
 
 		rpc("sync_position", global_position)
 
-
 @rpc("any_peer")
 func receive_input(dir: Vector2):
 	if multiplayer.is_server():
 		input_dir = dir
-
 
 @rpc("unreliable")
 func sync_position(pos: Vector2):
